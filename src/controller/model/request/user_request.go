434e60bee3db71c91edd65b07cad9634e84beab4
Diogo <diogo.alvesf88@gmail.com>
2025-07-27 23:43:02 -0300

    feat: validando campos da requisição

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -1,7 +1,7 @@
 package request
 
 type UserRequest struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Name     string `json:"name" binding:"required,min=4,max=100"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6,containsany=!@#$%^&*()_+"`
 }
