c912c4dc387be7b845c64029fcb32c310ecf3fd8
Diogo <diogo.alvesf88@gmail.com>
2025-07-28 21:36:26 -0300

    feat: adicionando logs ao projeto

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,15 +1,24 @@
 package controller
 
 import (
+	"github.com/diogoalvesf/my-routine-backend/src/configuration/logger"
 	"github.com/diogoalvesf/my-routine-backend/src/configuration/validation"
 	"github.com/diogoalvesf/my-routine-backend/src/controller/model/request"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller",
+		zap.String("journey", "createUser"),
+	)
+
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "createUser"))
+
 		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
